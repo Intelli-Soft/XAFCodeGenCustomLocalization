2bea8d6f23336b971046a9e54d9d36136df4ba28
Harald Bacik <h.bacik@intelli-soft.at>
2022-11-27 14:58:14 +0100

    Id in XAFML is able to work with whitespaces. They must be removed before writing Namespaces, Property names

--- XAFCodeGenCustomLocalization/CodeGenerator/Common.cs
+++ XAFCodeGenCustomLocalization/CodeGenerator/Common.cs
@@ -18,7 +18,10 @@ namespace XAFCodeGenCustomLocalization.CodeGenerator
                     {
                         locLocalizationNaming = new LocalizationNaming();
 
-                        locLocalizationNaming.GroupName = string.Format(@$"{GroupName}\{nodeData.NodeName}");
+                        var locGroupName = RemoveWhiteSpacesAndSetFirstLetterUppercase(GroupName);
+                        var locNodeName = RemoveWhiteSpacesAndSetFirstLetterUppercase(nodeData.NodeName);
+
+                        locLocalizationNaming.GroupName = string.Format(@$"{locGroupName}\{locNodeName}");
                         locLocalizationNaming.PropertyName = locPropertyName.PropertyName;
                         yield return locLocalizationNaming;
                     }
@@ -67,6 +70,29 @@ namespace XAFCodeGenCustomLocalization.CodeGenerator
             }
 
             return locReturnList;
+        }
+
+        internal static string RemoveWhiteSpacesAndSetFirstLetterUppercase(string name)
+        {
+            var locStrings = name.Trim().Split(" ");
+            
+            if (locStrings.Length > 1 ) {
+                //Let the first letter as the user wants - maybe it should be lowercase
+                for (int locItem = 1; locItem < locStrings.Count(); locItem++)
+                {
+                    if (locStrings[locItem].Length == 0)
+                        throw new Exception("Empty word?");
+                    else if (locStrings[locItem].Length == 1)
+                        locStrings[locItem] = char.ToUpper(locStrings[locItem][0]).ToString();
+                    else
+                        locStrings[locItem] = char.ToUpper(locStrings[locItem][0]) + locStrings[locItem].Substring(1);
+                }
+
+            }
+
+            return string.Join("", locStrings);
+
+
         }
 
         internal static string GetTempFile()
